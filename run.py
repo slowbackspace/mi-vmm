659a416549cd9f87f259968bb0840ee8b7d06424
slowbackspace <slowbackspace@gmail.com>
2016-12-07 17:32:11 +0100

    result page wip

--- run.py
+++ run.py
@@ -15,30 +15,48 @@ app.config.update(
     SECRET_KEY = 'e535b3c89a1288055e335fba78cc5b84c5a8ef5620b6b3f1'
 )
 
-# controllers
+
+def parse_float_or_none(value):
+    if value == "":
+        return None
+    else:
+        return float(value)
+
+
+def parse_float_or_zero(value):
+    if value == "":
+        return None
+    else:
+        return 0
+
+
 @app.route('/')
 def index_page():
     return render_template('index.html')
 
+
 @app.route('/result/', methods=["POST"])
 def result_page():
-    keyword = request.form.get("keyword", None)
-    date = request.form.get("date", None)
-    duration = float(request.form.get("duration", None))
-    views = float(request.form.get("views", None))
-    lat, lon = float(request.form.get("lat", None)), float(request.form.get("lon", None))
-    weight_views = float(request.form.get("weight-views", 0))
-    weight_date = float(request.form.get("weight-date", 0))
-    weight_duration = float(request.form.get("weight-duration", 0))
-    weight_location = float(request.form.get("weight-location", 0))
+    keyword = request.form.get("keyword")
+    date = request.form.get("date") or None
+    duration = parse_float_or_none(request.form.get("duration"))
+    views = parse_float_or_none(request.form.get("views"))
+    lat, lon = parse_float_or_none(request.form.get("lat")), parse_float_or_none(request.form.get("lon"))
+    weight_views = float(request.form.get("weight-views"))
+    weight_date = float(request.form.get("weight-date"))
+    weight_duration = float(request.form.get("weight-duration"))
+    weight_location = float(request.form.get("weight-location"))
 
-    # call reranking function
-    datetime_obj = datetime.datetime.strptime(date, "%m/%d/%Y").replace(tzinfo=datetime.timezone.utc)
+    if date:
+        datetime_obj = datetime.datetime.strptime(date, "%m/%d/%Y").replace(tzinfo=datetime.timezone.utc)
+    else:
+        datetime_obj = None
+
     result = reranking.search(keyword=keyword, length=duration, lengthW=weight_duration, views=views,
                     viewsW=weight_views, location = (lat, lon), locW=weight_location,
                     date=datetime_obj, dateW=weight_date)
 
-    return str(result)
+    return render_template("vids.html", result=result)
 
 
 # special file handlers
@@ -46,10 +64,7 @@ def result_page():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'img/favicon.ico')
 
-# error handlers
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+
 
 # server launchpad
 if __name__ == '__main__':
