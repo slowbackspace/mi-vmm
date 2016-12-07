c03cdb0ec3973f4aff1e1fea8a8ebebc37f9e59b
slowbackspace <slowbackspace@gmail.com>
2016-12-07 01:12:10 +0100

    WIP

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ import os
 import json
 from flask import Flask
 from flask import render_template, send_from_directory, Response, url_for, request
+import reranking
+import datetime
 
 # app initialization
 app = Flask(__name__)
@@ -22,17 +24,21 @@ def index_page():
 def result_page():
     keyword = request.form.get("keyword", None)
     date = request.form.get("date", None)
-    duration = request.form.get("duration", None)
-    views = request.form.get("views", None)
-    lat, lon = request.form.get("lat", None), request.form.get("lon", None)
-    weight_views = request.form.get("weight-views", 0)
-    weight_date = request.form.get("weight-date", 0)
-    weight_duration = request.form.get("weight-duration", 0)
-    weight_location = request.form.get("weight-location", 0)
+    duration = float(request.form.get("duration", None))
+    views = float(request.form.get("views", None))
+    lat, lon = float(request.form.get("lat", None)), float(request.form.get("lon", None))
+    weight_views = float(request.form.get("weight-views", 0))
+    weight_date = float(request.form.get("weight-date", 0))
+    weight_duration = float(request.form.get("weight-duration", 0))
+    weight_location = float(request.form.get("weight-location", 0))
 
     # call reranking function
+    datetime_obj = datetime.datetime.strptime(date, "%m/%d/%Y").replace(tzinfo=datetime.timezone.utc)
+    result = reranking.search(keyword=keyword, length=duration, lengthW=weight_duration, views=views,
+                    viewsW=weight_views, location = (lat, lon), locW=weight_location,
+                    date=datetime_obj, dateW=weight_date)
 
-    return "result"
+    return str(result)
 
 
 # special file handlers
