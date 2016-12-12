63d3848b5fc6076d99308f020376ec39ac15315e
Oliver Keruľ-Kmec <keruloli@fit.cvut.cz>
2016-12-12 23:25:22 +0100

    add comments and refactoring

--- reranking.py
+++ reranking.py
@@ -10,14 +10,12 @@ from geopy.distance import great_circle
 from math import radians, cos, sin, asin, sqrt
 
 
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
+# YouTube API KEY
 DEVELOPER_KEY = "AIzaSyAYMg3XQ2J1trdyr5jgypW-Dbn4xufRPS0"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+# great cirle distance
 def great_circle_distance(lat1, lon1, lat2, lon2):
     # convert to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
@@ -29,12 +27,13 @@ def great_circle_distance(lat1, lon1, lat2, lon2):
     meters = 6367 * angle * 1000
     return meters
 
-
+# min max normalization
 def normalize(value, minX, maxX):
     if maxX == minX:
         return 1
     return (value-minX)/(maxX-minX)
 
+# set min and max of 3 numbers
 def find_min_max(value, act_min, act_max):
     if value < act_min:
         act_min = value
@@ -43,17 +42,24 @@ def find_min_max(value, act_min, act_max):
     return act_min, act_max
 
 
+# count distance between 2 videos
 def video_distance(video, location, locW, views, viewsW, date, dateW, length, lengthW, minDistances, maxDistances):
+    # intialise distances
     locDistanceNorm = viewsDistanceNorm = dateDistanceNorm = lengthDistanceNorm = 0
+
+    # location distance
     if locW != 0:
         locDistance = great_circle_distance(video["location"][0],video["location"][1],location[0],location[1])
         locDistanceNorm = normalize(locDistance, minDistances["location"], maxDistances["location"])
+    # views distance
     if viewsW != 0:
         viewsDistance = abs(int(video["views"]) - views)
         viewsDistanceNorm = normalize(viewsDistance, minDistances["views"], maxDistances["views"])
+    # date distance
     if dateW != 0:
         dateDistance = abs(isodate.parse_datetime(video["date"]) - date).total_seconds()
         dateDistanceNorm = normalize(dateDistance, minDistances["date"], maxDistances["date"])
+    # duration distance
     if lengthW != 0:
         durationSec = int(video["length"])
         lengthDistance = abs(durationSec - length)
@@ -62,6 +68,85 @@ def video_distance(video, location, locW, views, viewsW, date, dateW, length, le
     return (locDistanceNorm * locW + viewsDistanceNorm * viewsW 
             + dateDistanceNorm * dateW + lengthDistanceNorm * lengthW)
 
+def get_videos_response(youtube,keyword,location, locW):
+    search_response = youtube.search().list(
+        q=keyword,
+        type="video",
+        part="id,snippet",
+        location=None if locW==0 else ",".join(map(str, location)),
+        locationRadius=None if locW==0 else "1000km",
+        maxResults=50
+    ).execute()
+    return search_response  
+
+def set_min_max(videos, location, views, date, length, locW, viewsW, dateW, lengthW):
+    # initialise min and max distances
+    minDistances = {
+    "location" : 0,
+    "views" : 0,
+    "date" : 0,
+    "length" :0
+    }
+
+    maxDistances = {
+    "location" : 0,
+    "views" : 0,
+    "date" : 0,
+    "length" :0
+    }
+
+    # initialise min and max distances with distance of first video
+    if locW > 0:
+        minDistances["location"] = maxDistances["location"] = great_circle_distance(videos[0]["location"][0],videos[0]["location"][1],location[0],location[1])
+
+    if viewsW > 0:
+        minDistances["views"] = maxDistances["views"] = abs(int(videos[0]["views"]) - views)
+
+    if dateW > 0:
+        minDistances["date"] = maxDistances["date"] = abs(isodate.parse_datetime(videos[0]["date"]) - date).total_seconds()
+
+    if lengthW > 0:
+        durationSec = int(videos[0]["length"])
+        minDistances["length"] = maxDistances["length"] = abs(durationSec - length)
+
+    # find min and max distances
+    for video in videos:
+        if locW > 0:
+            act_distance = great_circle_distance(video["location"][0],video["location"][1],location[0],location[1])
+            minDistances["location"], maxDistances["location"] = find_min_max(act_distance, minDistances["location"], maxDistances["location"])
+
+        if viewsW > 0:
+            act_distance = abs(int(video["views"]) - views)
+            minDistances["views"], maxDistances["views"] = find_min_max(act_distance, minDistances["views"], maxDistances["views"])
+
+        if dateW > 0:
+            act_distance = abs(isodate.parse_datetime(video["date"]) - date).total_seconds()
+            minDistances["date"], maxDistances["date"] = find_min_max(act_distance, minDistances["date"], maxDistances["date"])
+
+        if lengthW > 0:
+            durationSec = int(video["length"])
+            act_distance = abs(durationSec - length)
+            minDistances["length"], maxDistances["length"] = find_min_max(act_distance, minDistances["length"], maxDistances["length"])
+
+    return minDistances, maxDistances
+
+def get_video(video_result, locW,i):
+    location_searched = None if locW==0 else (
+                            video_result["recordingDetails"]["location"].get("latitude",None),
+                            video_result["recordingDetails"]["location"].get("longitude",None)
+                            )
+    length_searched = video_result.get("contentDetails", {}).get("duration", None)
+    video = {
+        "seq" : i,
+        "url": "https://www.youtube.com/watch?v=" + video_result["id"],
+        "title": video_result["snippet"]["title"],
+        "thumbnail": video_result["snippet"]["thumbnails"]["default"]["url"],
+        "location": None if location_searched==(None,None) else location_searched,
+        "views": video_result.get("statistics", {}).get("viewCount", None),
+        "date": video_result.get("snippet", {}).get("publishedAt", None),
+        "length": None if length_searched is None else isodate.parse_duration(length_searched).total_seconds()
+        }
+    return video   
 
 def search(keyword, location=None, locW=0, views=None, viewsW=0, 
            date=None, dateW=0, length=None, lengthW=0):
@@ -77,25 +162,16 @@ def search(keyword, location=None, locW=0, views=None, viewsW=0,
     if length is None:
         lengthW = 0
 
-    # Call the search.list method to retrieve results matching the specified
-    # query term.
-    search_response = youtube.search().list(
-        q=keyword,
-        type="video",
-        part="id,snippet",
-        location=None if locW==0 else ",".join(map(str, location)),
-        locationRadius=None if locW==0 else "1000km",
-        maxResults=50
-    ).execute()
+    # search videos
+    search_response = get_videos_response(youtube,keyword,location,locW)
 
     search_videos = []
-    print(type(search_response))
     # Merge video ids
     for search_result in search_response.get("items", []):
         search_videos.append(search_result["id"]["videoId"])
     video_ids = ",".join(search_videos)
 
-    # Call the videos.list method to retrieve location details for each video.
+    # retrieve details for videos
     video_response = youtube.videos().list(
         id=video_ids,
         part="snippet,recordingDetails,statistics,contentDetails"
@@ -103,85 +179,20 @@ def search(keyword, location=None, locW=0, views=None, viewsW=0,
 
     videos = []
     i=0
-    # Add each result to the list, and then display the list of matching videos.
+    # create dictionary of videos
     for video_result in video_response.get("items", []):
         i += 1
-        location_searched = None if locW==0 else (
-                            video_result["recordingDetails"]["location"].get("latitude",None),
-                            video_result["recordingDetails"]["location"].get("longitude",None)
-                            )
-        length_searched = video_result.get("contentDetails", {}).get("duration", None)
-        video = {
-            "seq" : i,
-            "url": "https://www.youtube.com/watch?v=" + video_result["id"],
-            "title": video_result["snippet"]["title"],
-            "thumbnail": video_result["snippet"]["thumbnails"]["default"]["url"],
-            "location": None if location_searched==(None,None) else location_searched,
-            "views": video_result.get("statistics", {}).get("viewCount", None),
-            "date": video_result.get("snippet", {}).get("publishedAt", None),
-            "length": None if length_searched is None else isodate.parse_duration(length_searched).total_seconds()
-            }
+        video = get_video(video_result,locW,i)
         if video["location"] is None:
             locW=0
         videos.append(video)
 
-    minDistances = {
-    "location" : 0,
-    "views" : 0,
-    "date" : 0,
-    "length" :0
-    }
-
-    maxDistances = {
-    "location" : 0,
-    "views" : 0,
-    "date" : 0,
-    "length" :0
-    }
-
-    if locW > 0:
-        minDistances["location"] = maxDistances["location"] = great_circle_distance(videos[0]["location"][0],videos[0]["location"][1],location[0],location[1])
-
-    if viewsW > 0:
-        minDistances["views"] = maxDistances["views"] = abs(int(videos[0]["views"]) - views)
-
-    if dateW > 0:
-        minDistances["date"] = maxDistances["date"] = abs(isodate.parse_datetime(videos[0]["date"]) - date).total_seconds()
-
-    if lengthW > 0:
-        durationSec = int(videos[0]["length"])
-        minDistances["length"] = maxDistances["length"] = abs(durationSec - length)
-
-    for video in videos:
-        if locW > 0:
-            act_distance = great_circle_distance(video["location"][0],video["location"][1],location[0],location[1])
-            minDistances["location"], maxDistances["location"] = find_min_max(act_distance, minDistances["location"], maxDistances["location"])
-
-        if viewsW > 0:
-            act_distance = abs(int(video["views"]) - views)
-            print(act_distance)
-            minDistances["views"], maxDistances["views"] = find_min_max(act_distance, minDistances["views"], maxDistances["views"])
-
-        if dateW > 0:
-            act_distance = abs(isodate.parse_datetime(video["date"]) - date).total_seconds()
-            minDistances["date"], maxDistances["date"] = find_min_max(act_distance, minDistances["date"], maxDistances["date"])
-
-        if lengthW > 0:
-            durationSec = int(video["length"])
-            act_distance = abs(durationSec - length)
-            minDistances["length"], maxDistances["length"] = find_min_max(act_distance, minDistances["length"], maxDistances["length"])
-
-
+    minDistances, maxDistances = set_min_max(videos, location, views, date, length, locW, viewsW, dateW, lengthW)
 
     unsorted_videos = videos.copy()
+    # sort videos
     videos.sort(key=lambda video: video_distance(video, location, locW, 
                 views, viewsW, date, dateW, length, lengthW, minDistances, maxDistances))
-
-    for video in unsorted_videos:
-        print(video)
-    print ("\n\n\n\n")
-    for video in videos:
-        pprint(video)
 
     return unsorted_videos, videos
 
